687ee44724b3bd61803b46fbac27bd735c1cf939
Leo Jim <leojim06@gmail.com>
2017-11-30 20:12:59 -0500

    feat(nav-info): Navegación e información
    Se implementa la navegación por rutas y se prueba con la creación de un componente home.
    
    Se agregan el toaster de notificaciones y el dialog para solicitar permos al usuario, además de que se incluyen todas las dependencias necesarias para el proyecto (ngrx, socket.io)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToasterConfig, ToasterService } from 'angular2-toaster';
+import { Modal } from 'ngx-modialog/plugins/vex';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  toasterconfig: ToasterConfig = new ToasterConfig({
+    animation: 'fade',
+    timeout: 6000,
+    preventDuplicates: true,
+    mouseoverTimerStop: true
+  });
+
+  constructor(
+    private modal: Modal,
+    private toasterService: ToasterService
+  ) { }
+
+  onClick() {
+    this.modal.confirm()
+      .isBlocking(true)
+      .showCloseButton(false)
+      .keyboard(27)
+      .okBtn('Aceptar')
+      .message('Este es el boton de alerta')
+      .open()
+      .result
+      .then(result => this.toasterService.pop('success', 'Hola', 'Toaster service'))
+      .catch(result => this.toasterService.pop('error', 'Hola', 'Toaster Service'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ToasterModule } from 'angular2-toaster';
+import { ModalModule } from 'ngx-modialog';
+import { VexModalModule } from 'ngx-modialog/plugins/vex';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot(),
+    VexModalModule,
+    ToasterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
